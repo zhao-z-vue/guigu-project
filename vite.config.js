9a187469839df91b561e51e93a01a773ad5713fa
zz <pagenaxiya@qq.com>
2026-08-19 16:57:30 +0800

    fix: 强制启用Mock以便线上展示

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,8 @@ export default defineConfig(({ mode }) => {
         mockPath: 'mock',
         enable: mode === 'development', // 改用 mode 判断
         logger: true,
+        localEnabled: true,  // 原来是 command === 'serve'，改成 true
+        prodEnabled: true,   // 新增这行，让生产环境也启用
       }),
     ],
     resolve: {
